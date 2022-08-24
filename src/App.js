02311f0e4521f37ed4f722f4518aced838ad3fa6
Xavski <xavioursaveski1@gmail.com>
2022-08-25 00:32:47 +1000

    Editing cart

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import Discounted from "./components/Discounted";
-import Explore from "./components/Explore";
-import Featured from "./components/Featured";
 import Footer from "./components/Footer";
-import Highlights from "./components/Highlights";
-import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Books from "./pages/Books";
+import { books } from "./data";
+import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
+  
   return (
-    <div className="App">
-      <Nav/>
-      <Landing/>
-      <Highlights/>
-      <Featured/>
-      <Discounted/>
-      <Explore/>
-      <Footer/>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          {/* exact where path cannot be altered and output same page */}
+          <Route path="/books" element={<Books books={books} />} />
+          {/* send props to route */}
+          <Route path="/books/:id" element={<BookInfo books={books} />} />
+          <Route path="/cart" element={<Cart books={books}/>}/>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
